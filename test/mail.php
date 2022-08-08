166d917d978d83131ea6ea1128f224637710e496
FDIL Yassine <109689084+fdilyassine@users.noreply.github.com>
2022-08-08 16:29:55 +0100

    Add files via upload

--- test/mail.php
+++ test/mail.php
@@ -1,36 +1,57 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
 
-$mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "ichfak.ichfak99@gmail.com";
+if(isset($_POST['submit'])){
 
-mail($recipient, $subject, $message, $mailheader);// or die("Error!");
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $recipient = "dotaki2368@aregods.com";
 
-echo'
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
-    </div>
-</body>
-</html>
-';
+
+    $mailheader = "From:".$name."<".$email.">\r\n";
+
+    mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+    header("Location: index.php?mailsend");
+
+
+
+
+    echo'
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Contact form</title>
+            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <div class="container">
+                <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+                <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+                
+            </div>
+        </body>
+        </html>
+        ';
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
